3abcb65c5ba39f2a3c916512b3c197639f466f33
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-06-01 15:34:07 +0200

    [1] Add Test.swift

---
+++ Jointify/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  Jointify
+//
+//  Created by Lukas Gerhardt on 01.06.20.
+//  Copyright © 2020 Lukas Gerhardt. All rights reserved.
+//
+
+// MARK: Imports
+import Foundation
+
+// MARK: - Tests
+struct Test {
+    let number: Int
+    let name: String
+}
