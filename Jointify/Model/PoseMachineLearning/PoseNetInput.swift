7f5df10f2e210354328a22270e212b805fb1c818
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-06-15 20:56:24 +0200

    Make PoseNetInput conform to style guide

--- Jointify/Model/PoseMachineLearning/PoseNetInput.swift
+++ Jointify/Model/PoseMachineLearning/PoseNetInput.swift
@@ -6,35 +6,42 @@ The implementation of a CoreML feature provider passed to the PoseNet model by t
  PoseNet class for prediction.
 */
 
+// MARK: Imports
 import CoreML
 import Vision
 
+// MARK: - PoseNetInput
 /// - Tag: PoseNetInput
 class PoseNetInput: MLFeatureProvider {
+    
+    // MARK: Stored Type Properties
     /// The name of the PoseNet model's input feature.
     ///
     /// You can see all the model's inputs and outputs, their names, and other information by selecting
     /// `PoseNetMobileNet075S16FP16.mlmodel` in the Project Navigator.
     private static let imageFeatureName = "image"
-
-    /// The original image this feature provider uses as input to its consumers.
-    var imageFeature: CGImage
-
+    
+    // MARK: Stored Instance Properties
     /// The size of the images this feature provider produces to its consumers.
     ///
     /// The feature provider resizes the `imageFeature` property by
     /// cropping and resizing it to this size in the `featureValue(for:)` method.
     let imageFeatureSize: CGSize
+    
+    /// The original image this feature provider uses as input to its consumers.
+    var imageFeature: CGImage
 
     var featureNames: Set<String> {
         return [PoseNetInput.imageFeatureName]
     }
-
+    
+    // MARK: Initializers
     init(image: CGImage, size: CGSize) {
         imageFeature = image
         imageFeatureSize = size
     }
-
+    
+    // MARK: Instance Methods
     func featureValue(for featureName: String) -> MLFeatureValue? {
         guard featureName == PoseNetInput.imageFeatureName else {
             return nil
