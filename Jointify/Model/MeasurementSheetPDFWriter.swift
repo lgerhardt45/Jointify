3d081bd803fc151c747358afb27072cc72e895a7
Lennart <l.jayasuriya@tum.de>
2020-06-20 16:02:58 +0200

    Fix colon violation conform to style guide

--- Jointify/Model/MeasurementSheetPDFWriter.swift
+++ Jointify/Model/MeasurementSheetPDFWriter.swift
@@ -147,7 +147,10 @@ class MeasurementSheetPDFWriter {
             let scale = UIScreen.main.scale
             UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
 
-            let textFontAttributes = [NSAttributedString.Key.font: textFont, NSAttributedString.Key.foregroundColor: textColor] as [NSAttributedString.Key : Any]
+            let textFontAttributes =
+                [NSAttributedString.Key.font: textFont,
+                 NSAttributedString.Key.foregroundColor: textColor]
+                    as [NSAttributedString.Key: Any]
             image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
 
             let rect = CGRect(origin: point, size: image.size)
