fac9b0bad5a2f16fe11660d0204dd235ea479fb5
Annalena Feix <annalena.feix@tum.de>
2020-06-23 10:28:58 +0200

    Added DateExtension

---
+++ Jointify/TypeExtensions/DateExtension.swift
@@ -0,0 +1,38 @@
+//
+//  DateExtension.swift
+//  Jointify
+//
+//  Created by Annalena Feix on 23.06.20.
+//  Copyright © 2020 Lukas Gerhardt. All rights reserved.
+//
+
+// MARK: Imports
+import Foundation
+
+//MARK: -DateExtension
+struct DateFormats {
+    
+    // Adapted from https://www.ralfebert.de/ios/swift-dateformatter-datumsangaben-formatieren/
+    static var dateOnlyFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    static var timeOnlyFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .none
+        return formatter
+    }()
+    
+}
+
+extension DefaultStringInterpolation {
+    
+    mutating func appendInterpolation(_ value: Date, formatter: DateFormatter) {
+        self.appendInterpolation(formatter.string(from: value))
+    }
+    
+}
