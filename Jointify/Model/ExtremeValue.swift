f612adb286fa6dbc390415343178c7ceb38a5000
Niklas Bergmüller <niklas.bergmueller@preisenergie.de>
2020-06-25 22:18:27 +0200

    Add ExtremeValues Enum

---
+++ Jointify/Model/ExtremeValue.swift
@@ -0,0 +1,15 @@
+//
+//  ExtremeValue.swift
+//  Jointify
+//
+//  Created by Niklas Bergmüller on 25.06.20.
+//  Copyright © 2020 Lukas Gerhardt. All rights reserved.
+//
+
+import Foundation
+
+// Enum for the to different extreme values we want to show to the patient
+enum ExtremeValue: String {
+    case minimum
+    case maximum
+}
