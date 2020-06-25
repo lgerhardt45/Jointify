77f5fd4717563b0129884e3b4e10f05167f87972
Lennart <l.jayasuriya@tum.de>
2020-06-25 20:05:38 +0200

    Embed InfoView in ProcessingView, remove subheadline "please wait"

--- Jointify/Views/Screens/ProcessingView.swift
+++ Jointify/Views/Screens/ProcessingView.swift
@@ -45,20 +45,16 @@ struct ProcessingView: View {
                 
                 // 20% for the headline
                 LogoAndHeadlineView(
-                    headline: "Analyzing",
+                    headline: "Analyzing...",
                     showLogo: true,
                     allowToPopView: false,
                     height: geometry.size.height * 0.20
                 )
                 
-                // subheadline
-                SubHeadline(subheadline: "Please wait...", width: geometry.size.width / 2.0)
-                
                 // Placeholder
-                Text("Insert fun facts and info stuff here")
-                
-                Spacer()
-                
+                InfoView(width: geometry.size.width * 0.8)
+                    .padding(.vertical)
+                                    
                 ProgressBar(
                     currentProgress: self.$progress,
                     total: self.$total,
