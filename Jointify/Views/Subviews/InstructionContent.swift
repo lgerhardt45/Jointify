2db73f67744cf85005dc8530ef9fa8057455f5ca
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-06-26 21:55:30 +0200

    Add BulletedList struct that supports bulletpoints

--- Jointify/Views/Subviews/InstructionContent.swift
+++ Jointify/Views/Subviews/InstructionContent.swift
@@ -17,7 +17,45 @@ struct InstructionContent: View {
         VStack {
             Text("why the fuck would we add fifty lines of instructions?")
         }
+// MARK: - BulletedList
+struct BulletedList: View {
+    
+    // MARK: Stored Instance Properties
+    private let headline: String
+    private let text: [String]
+    
+    // MARK: Initializers
+    init(headline: String, text: [String]) {
+        self.headline = headline
+        self.text = text
+    }
+    
+    // MARK: Body
+    var body: some View {
         
+        VStack(spacing: 8) {
+            
+            // headline
+            Text(self.headline)
+                .font(.headline)
+            
+            VStack(alignment: .leading) {
+                
+                // bulletpoints
+                ForEach(self.text, id: \.self) { bulletpoint in
+                    
+                    HStack(alignment: .top) {
+                        Image(systemName: "circle.fill")
+                            .resizable()
+                            .frame(width: 5, height: 5)
+                            .padding(.top, 7.5)
+                        
+                        Text(bulletpoint)
+                            .font(.body)
+                    }
+                }
+            }
+        }
     }
 }
 
