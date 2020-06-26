8007cac11f520c8e3012def25c8629afa094a231
Lukas Gerhardt <lukas.gerhardt@tum.de>
2020-06-26 20:27:25 +0200

    Clean empty InstructionContent.swift

--- Jointify/Views/Subviews/InstructionContent.swift
+++ Jointify/Views/Subviews/InstructionContent.swift
@@ -15,44 +15,14 @@ struct InstructionContent: View {
     // MARK: Body
     var body: some View {
         VStack {
-            // Mocking with "llorem ipsum"
-            InstructionBulletPoint(number: 1)
-            InstructionBulletPoint(number: 2)
+            Text("why the fuck would we add fifty lines of instructions?")
         }
         
     }
 }
 
-// MARK: - InstructionBulletPoint
-private struct InstructionBulletPoint: View {
-    
-    // MARK: Constants
-    public enum Constants {
-        static let lloremIpsum = "Lorem ipsum dolor sit amet"
-    }
-    
-    // MARK: Stored Instance Properties
-    let number: Int
-    
-    // MARK: Body
-    var body: some View {
-        HStack(alignment: .top) {
-            Text("\(number).")
-            .fontWeight(.light)
-            .font(.system(size:18))
-                .fontWeight(.light)
-            Text(Constants.lloremIpsum)
-            .fontWeight(.light)
-            .font(.system(size:18))
-                .fontWeight(.light)
-            
-        }
-        .padding(.horizontal, 32.0)
-    }
-}
-
 struct InstructionContent_Previews: PreviewProvider {
     static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+        InstructionContent()
     }
 }
