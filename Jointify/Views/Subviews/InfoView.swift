d3c592a86961e122a0fab14766eb9f879e84ea60
Lennart <l.jayasuriya@tum.de>
2020-07-03 21:30:29 +0200

    no message

--- Jointify/Views/Subviews/InfoView.swift
+++ Jointify/Views/Subviews/InfoView.swift
@@ -19,32 +19,56 @@ struct InfoView: View {
     let displayDismissButton: Bool
     let width: CGFloat
     // swiftlint:disable line_length
-    private let infoMessage =
-    """
-    Your values are being measured by the "Neutral-Null-Methode", which is an orthopedic index for measuring joint mobility.
+    
+    //solution from https://stackoverflow.com/questions/59624838/swiftui-tappable-subtext
+    struct infoMessage: View {
+        
+        var body: some View {
+            
+            HStack {
+                
+                Text("""
 
-    Your mobility will be expressed in 3 angle degrees originating from the neutral-zero position, which is defined for each joint specifically. The neutral-zero position is what we referred to as your starting position earlier.
+The values you see are the maximum and minumum range of motion angle you were able to reach by straigthening and bending your joint.
 
-    From this position Jointify measures your maximum Range of Motion in both directions and sends it to your doctor, who is then able to determine, if your range of motion is impaired.
+            For the PDF sent to your doctor we will translate your values into the "Neutral-Zero-Method", which is an orthopedic index for measuring joint mobility.
 
-    Values:
-    1) Movement away from body
-    2) 0, if Neutral-Zero is achieved
-    3) Movement towards body
+            Your mobility will be expressed in 3 angle degrees originating from the neutral-zero position, which is defined for each joint specifically.
 
-    Disclaimer: Please note that Range of Motion values are highly individually dependent on your physical conditions and can only be correctly interpreted by a professional. Do not use these values to diagnose yourself.
-    (Jointify haftet nicht?)
+            Taking your knee as an example, the neutral-zero position would be a 180 degree straight joint.
 
-    (Quelle: https://flexikon.doccheck.com/de/Neutral-Null-Methode)
-    """
+            Especially during recovery, you might not reach the neutral-zero position, which will be shown in the PDF as well, depending which three values are written in the PDF.
+
+            Hint: If the middle number displays 0, you are able to reach the neutral zero position for the respective joint. :-)
+
+            Disclaimer: Please note that Range of Motion values are highly individually dependent on your physical conditions and can only be correctly interpreted by a professional. Do not use these values to diagnose yourself.
+
+            If your interested in how exactly the values are read by a doctor please visit:
+""")
+                
+                Text("link")
+                    .foregroundColor(.blue)
+                    .underline()
+                    .onTapGesture {
+                        let url = URL.init(string: "https://flexikon.doccheck.com/de/Neutral-Null-Methode")
+                        guard let infoURL = url, UIApplication.shared.canOpenURL(infoURL) else { return }
+                        UIApplication.shared.open(infoURL)
+                }
+            }
+        }
+    }
+    
+    //(Quelle: https://flexikon.doccheck.com/de/Neutral-Null-Methode)
+    
     // swiftlint:enable line_length
     
     // MARK: Body
     var body: some View {
         VStack {
+
             Text("What are my values?").font(.title)
-            
-            ScrollView {
+                        ScrollView {
+                // TO DO: hier irgendwie die struct einbinden, ich weiß nicht wie das geht
                 Text(infoMessage)
                     .padding()
             }
